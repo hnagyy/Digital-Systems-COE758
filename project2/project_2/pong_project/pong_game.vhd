1ceccb70f2e53e2cb20f7a6ad3a1291f87fb9d44
Habiba <146796712+hnagyy@users.noreply.github.com>
2026-03-24 21:46:54 -0400

    Update pong_game.vhd

--- project2/project_2/pong_project/pong_game.vhd
+++ project2/project_2/pong_project/pong_game.vhd
@@ -1,22 +1,4 @@
-----------------------------------------------------------------------------------
--- Company:
--- Engineer:
---
--- Create Date: 14:23:16 11/09/2025
--- Design Name:
--- Module Name: pong_game - Behavioral
--- Project Name:
--- Target Devices:
--- Tool versions:
--- Description:
---
--- Dependencies:
---
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
---
-----------------------------------------------------------------------------------
+
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.STD_LOGIC_ARITH.ALL;
